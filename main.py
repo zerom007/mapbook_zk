19b723d21c7e656241488d9267014f3f0a61854e
zerom007 <zerom.kisiel@student.wat.edu.pl>
2024-12-02 14:35:14 +0100

    feat: add create new user

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from mapbook.users import users
-from mapbook.crud import hello, read_users, add_user
+from mapbook.crud import hello, read_users, add_user, remove_user
 
 
 def main():
     hello(users[0]['name'])
     read_users(users)
-    add_user(users)
+    # add_user(users)
+    remove_user(users)
+    read_users(users)
+
+
 
 if __name__=='__main__':
     main()
--- mapbook/crud.py
+++ mapbook/crud.py
@@ -15,3 +15,10 @@ def add_user(userlist:list)->None:
     new_city: str=input('Podaj miasto ')
     new_user: dict = {'name': new_name, 'posts': new_posts, 'city': new_city}
     userlist.append(new_user)
+
+def remove_user(userlist: list) -> None:
+    user_to_find: str = input('Podaj imię do usuniecia:')
+    for user in users:
+         if user['name'] == user_to_find:
+            print(f'usuwam:{user}')
+            userlist.remove(user)
