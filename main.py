b049c57ba6154df80fdf4e3416b7052a86764550
zerom007 <zerom.kisiel@student.wat.edu.pl>
2024-12-09 12:21:39 +0100

    feat: menu add

--- main.py
+++ main.py
@@ -11,6 +11,30 @@ def main():
     read_users(users)
 
 
+while True:
+    print("======MENU=====")
+    print("1. Dodaj Znajomego")
+    print("2. Wczytaj Znajomego")
+    print("3. Aktualoizuj Znajomego")
+    print("4. Usuń Znajomego")
+    print("5. Generuj mapę z lokalizacją znajomego")
+    print("6. Generuj mapę z lokalizacją wszystkoch znajomych")
+
+
+    menu_option: str=input("Użytkowniku wybierz opcję menu: ")
+    print(f'Użytkownik wybrał {menu_option}')
+    if menu_option == '1':
+        add_user(users)
+    if menu_option == '2':
+        read_users(users)
+    if menu_option == '3':
+        update_user(users)
+    if menu_option == '4':
+      remove_user(users)
+    if menu_option == '5':
+      pass
+    if menu_option == '6':
+      pass
 
 if __name__=='__main__':
     main()
