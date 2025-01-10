81b7d3aa0741761c26caf3b72bccba67ebe71beb
zerom007 <zerom.kisiel@student.wat.edu.pl>
2025-01-10 09:32:32 +0100

    Feat:add user, remove user in tkinter

--- main.py
+++ main.py
@@ -19,14 +19,36 @@ class User:
         self.lokalizacja = lokalizacja
 
 users=[
-    User('aaa','aaa','1','aaa'),
-    User('bbb','bbb','2','bbb'),
-    User('ccc','ccc','3','ccc'),
+    # User('aaa','aaa','1','aaa'),
+    # User('bbb','bbb','2','bbb'),
+    # User('ccc','ccc','3','ccc'),
 ]
 def show_users():
-
+    listbox_lista_obiektow.delete(0,END)
     for idx, user in enumerate (users):
-        listbox_lista_obiektow.insert(idx,user.imie)
+        listbox_lista_obiektow.insert(idx,f'{user.imie} {user.nazwisko} {user.postow} {user.lokalizacja}')
+
+def add_user()->None:
+    name=entry_imie.get()
+    surname=entry_nazwisko.get()
+    posts=entry_liczba_postow.get()
+    location=entry_lokalizacja.get()
+
+    new_user=User(name,surname,posts,location)
+
+    users.append(new_user)
+    show_users()
+    entry_imie.delete(0,END)
+    entry_nazwisko.delete(0,END)
+    entry_liczba_postow.delete(0,END)
+    entry_lokalizacja.delete(0,END)
+    entry_imie.focus()
+
+def delete_user()->None:
+    i=listbox_lista_obiektow.index(ACTIVE)
+    print(i)
+    users.pop(i)
+    show_users()
 
 
 
@@ -40,12 +62,12 @@ root.title('mapbook')
 ramka_lista_obiektow=Frame(root)
 ramka_formularz = Frame(root)
 ramka_szczegoly_obiektu = Frame(root)
+ramka_mapa=Frame(root)
 
 ramka_lista_obiektow.grid(row=0, column=0, padx=50)
 ramka_formularz.grid(row=0, column=1)
 ramka_szczegoly_obiektu.grid(row=1, column=0, padx=50, pady=20, columnspan=2)
 
-ramka_mapa=Frame(root)
 ramka_mapa.grid(row=2, column=0, padx=50, pady=20)
 
 # ramka_lista_obiektow
@@ -55,7 +77,7 @@ listbox_lista_obiektow=Listbox(ramka_lista_obiektow, width=50)
 listbox_lista_obiektow.grid(row=1, column=0, columnspan=3)
 button_pokaz_sczegoly=Button(ramka_lista_obiektow,text='Pokaz sczegoly',command=lambda:print('aaa'))
 button_pokaz_sczegoly.grid(row=2, column=0)
-button_usun_obiekt=Button(ramka_lista_obiektow,text='Usun obiekt')
+button_usun_obiekt=Button(ramka_lista_obiektow,text='Usun obiekt',command=delete_user)
 button_usun_obiekt.grid(row=2, column=1)
 button_edytuj_obiekt=Button(ramka_lista_obiektow,text='Edytuj obiekt')
 button_edytuj_obiekt.grid(row=2, column=2)
@@ -80,7 +102,7 @@ label_lokalizacja.grid(row=4, column=0,sticky=W)
 entry_lokalizacja=Entry(ramka_formularz)
 entry_lokalizacja.grid(row=4, column=1)
 
-button_dodaj_obiekt=Button(ramka_formularz,text='Dodaj obiekt')
+button_dodaj_obiekt=Button(ramka_formularz,text='Dodaj obiekt', command=add_user)
 button_dodaj_obiekt.grid(row=5, column=1,columnspan=2)
 
 
@@ -110,7 +132,7 @@ map_widget.set_position(52.0,21)
 map_widget.set_zoom(6)
 map_widget.grid(row=3, column=0,columnspan=8)
 
-show_users()
+
 
 
 
